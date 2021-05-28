4acdbee16b19987111151beab6551ed950a45452
alambrow <alexander.lambrow@gmail.com>
2021-05-28 14:40:29 -0500

    book 4 complete

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,9 +1,10 @@
 import React from "react"
-import { Animal } from "./animal/Animal.js"
 import { Employee } from "./employee/Employee.js"
 import { Location } from "./location/Location.js"
 import { Customer } from "./customer/Customer.js"
 import "./Kennel.css"
+import { AnimalProvider } from "./animal/AnimalProvider.js"
+import { AnimalList } from "./animal/AnimalList.js"
 
 export const Kennel = () => (
     <>
@@ -16,9 +17,9 @@ export const Kennel = () => (
 
         <h2>Animals</h2>
         <article className="animals">
-            <Animal />
-            <Animal />
-            <Animal />
+        <AnimalProvider>
+            <AnimalList />
+        </AnimalProvider>
         </article>
 
         <h2>Employees</h2>
---
+++ src/components/animal/AnimalList.js
@@ -0,0 +1,35 @@
+import React, { useContext, useEffect } from "react"
+import { AnimalContext } from "./AnimalProvider.js"
+import "./Animal.css"
+
+export const AnimalList = () => {
+    // temp state to store animals data
+    const { animals, getAnimals } = useContext(AnimalContext)
+
+    // invocation of react's useEffect() hook, which grabs data that cannot be handled at init render
+    // empty array at end is dependency array; it prevents infinite loop by setting the function depedent on iterating on empty array
+    useEffect(() => {
+        console.log("AnimalList: useEffect - getAnimals")
+        getAnimals()
+    }, [])
+
+    return (
+        <section className="animals">
+            {console.log("AnimalList: Render", animals)}
+            {
+                animals.map(animal => {
+                    return (
+                        <div className="animal" id={`animal--${animal.id}`}>
+                        <div className="animal__name">
+                          Name: { animal.name }
+                        </div>
+                        <div className="animal__breed">
+                          Breed: { animal.breed }
+                        </div>
+                        </div>
+                    )
+                })
+            }
+        </section>
+    )
+}
